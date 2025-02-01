f188e04ed8748eed0be49a64826df3ccf6871156
Arbitermartin <arbitermartin04@gmail.com>
2025-02-01 20:41:52 +0300

    add  new in my page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -17,3 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+// it direct thank you page for user input
+const params = new URLSearchParams(window.location.search);
+document.getElementById("first-name").textContent = params.get("first-name") || "N/A";
+document.getElementById("last-name").textContent = params.get("last-name") || "N/A";
+document.getElementById("email").textContent = params.get("email") || "N/A";
+document.getElementById("phone").textContent = params.get("phone") || "N/A";
+document.getElementById("organization").textContent = params.get("organization") || "N/A";
+document.getElementById("membership").textContent = params.get("membership") || "N/A";
+document.getElementById("timestamp").textContent = params.get("timestamp") || "N/A";
+
+// timestamp page
+document.getElementById('timestamp').value = new Date().toISOString();
+function openModal(id) {
+    document.getElementById(id).style.display = 'block';
+}
