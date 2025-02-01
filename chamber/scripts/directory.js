8dd6c4e6ab600325cb2f9fd585f58103650e94f2
Arbitermartin <arbitermartin04@gmail.com>
2025-02-01 13:04:28 +0300

    add new pages

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -225,3 +225,24 @@ year.innerHTML = `<span>${today.getFullYear()}</span>`;
 //last modified
 const date = document.querySelector("#lastModified");
 date.innerHTML = `<span>${document.lastModified}<span>`;
+
+// dialog button 
+document.addEventListener("DOMContentLoaded", function () {
+    const openButtons = document.querySelectorAll(".openButton, .openButton_1, .openButton_2, .openButton_3");
+    const closeButtons = document.querySelectorAll(".closeButton, .closeButton_1, .closeButton_2, .closeButton_3");
+    const modals = document.querySelectorAll(".dialogBox");
+
+    openButtons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            modals[index].style.display = "block";
+        });
+    });
+
+    closeButtons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            modals[index].style.display = "none";
+        });
+    });
+});
+
+
