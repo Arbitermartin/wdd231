f28463ff3261a1bc909cb9bda830aba5a1870a8a
Arbitermartin <arbitermartin04@gmail.com>
2025-02-18 00:29:12 +0300

    add changes

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -3,9 +3,9 @@
 document.getElementById('toggleButton').addEventListener('click',function(){
     let navMenu = document.getElementById('navMenu');
     if (navMenu.classList.contains('show')) {
-        this.innerHTML = ' <i class="fa-light fa-bars"></i>';
+        this.innerHTML = ' <i class="fa fa-bars"></i>';
     } else {
-        this.innerHTML = '<i class="fa-duotone fa-regular fa-x"></i>';
+        this.innerHTML = '<i class="fa fa-x"></i>';
     }
     navMenu.classList.toggle('show');
 });
